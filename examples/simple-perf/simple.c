1d648295c4cf92f9d1ea80b6bbd8faae024f8683
David Barina <ibarina@fit.vutbr.cz>
2013-06-25 16:23:34 +0000

    revert

--- examples/simple-perf/simple.c
+++ examples/simple-perf/simple.c
@@ -16,8 +16,7 @@ int main()
 	dwt_util_set_accel(12);
 
 	// image size
-	//const int x = 1920, y = 1080;
-	const int x = 256, y = 256;
+	const int x = 1920, y = 1080;
 
 	// size of picture element
 	const int stride_y = sizeof(float);
