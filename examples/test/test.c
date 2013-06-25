a322e022cc2b02c04412289a62bf2beb4e54da8f
David Barina <ibarina@fit.vutbr.cz>
2013-06-25 16:08:01 +0000

    updated test example

--- examples/test/test.c
+++ examples/test/test.c
@@ -14,12 +14,13 @@ int main()
 
 	const int x = 256, y = 256;
 
-	const int stride_y = sizeof(float);
-	const int stride_x = dwt_util_get_opt_stride(stride_y * x);
-
 	dwt_util_log(LOG_INFO, "Using %i threads.\n", dwt_util_get_num_threads());
 	dwt_util_log(LOG_INFO, "Using %i workers.\n", dwt_util_get_num_workers());
 	dwt_util_log(LOG_INFO, "Using image of size of %ix%i pixels.\n", x, y);
+
+	int j = -1;
+
+	dwt_util_log(LOG_INFO, "Testing: CDF 9/7, 2D, float...\n");
 
 	for(int accel = 0; accel <= 16; accel++)
 	{
@@ -30,25 +31,26 @@ int main()
 
 		dwt_util_log(LOG_INFO, "Acceleration type is %i.\n", dwt_util_get_accel());
 
-		int j = -1;
-
-		int ret = dwt_util_test_cdf97_2_s(
-			stride_x,
-			stride_y,
-			x,
-			y,
-			x,
-			y,
-			j,
-			0,
-			0
-		);
-
-		if( ret )
+		if( dwt_util_test2_cdf97_2_s(DWT_ARR_SIMPLE, x, y, 1, j, 1) )
 			dwt_util_log(LOG_INFO, "fail\n");
 		else
 			dwt_util_log(LOG_INFO, "success\n");
 	}
+
+	dwt_util_log(LOG_INFO, "Testing: CDF 9/7, 2D, double...\n");
+
+	if( dwt_util_test2_cdf97_2_d(DWT_ARR_SIMPLE, x, y, 1, j, 1) )
+		dwt_util_log(LOG_INFO, "fail\n");
+	else
+		dwt_util_log(LOG_INFO, "success\n");
+
+	dwt_util_log(LOG_INFO, "Testing: CDF 9/7, 2D, int...\n");
+
+	if( dwt_util_test2_cdf97_2_i(DWT_ARR_SIMPLE, x, y, 1, j, 1) )
+		dwt_util_log(LOG_INFO, "fail\n");
+	else
+		dwt_util_log(LOG_INFO, "success\n");
+
 
 	dwt_util_finish();
 
