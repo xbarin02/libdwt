583d56a9c57bdb850eb63d594746070738d424f5
David Barina <ibarina@fit.vutbr.cz>
2014-07-11 10:27:40 +0000

    added inverse single-loop core transform with symmetric border extension

--- src/dwt-sym.c
+++ src/dwt-sym.c
@@ -82,6 +82,79 @@ void vert_2x4(
 }
 #endif
 
+// vert_2x4 inv.
+#ifdef __SSE__
+static
+void vert_2x4_inv(
+	// left input column [4]
+	__m128 in0,
+	// right input column [4]
+	__m128 in1,
+	// output 0 [4]
+	__m128 *out0,
+	// output 1 [4]
+	__m128 *out1,
+	// 4x buffer "L" with stride = (1*4) * sizeof(float)
+	float *buff
+)
+{
+	// weights
+	const __m128 w0 = { +dwt_cdf97_p1_s, +dwt_cdf97_p1_s, +dwt_cdf97_p1_s, +dwt_cdf97_p1_s };
+	const __m128 w1 = { -dwt_cdf97_u1_s, -dwt_cdf97_u1_s, -dwt_cdf97_u1_s, -dwt_cdf97_u1_s };
+	const __m128 w2 = { +dwt_cdf97_p2_s, +dwt_cdf97_p2_s, +dwt_cdf97_p2_s, +dwt_cdf97_p2_s };
+	const __m128 w3 = { -dwt_cdf97_u2_s, -dwt_cdf97_u2_s, -dwt_cdf97_u2_s, -dwt_cdf97_u2_s };
+
+	// variables
+	__m128 l0, l1, l2, l3;
+	__m128 c0, c1, c2, c3;
+	__m128 r0, r1, r2, r3;
+	__m128 x0, x1;
+	__m128 y0, y1;
+
+	// load "L"
+	l0 = _mm_load_ps(&buff[0*(1*4)]);
+	l1 = _mm_load_ps(&buff[1*(1*4)]);
+	l2 = _mm_load_ps(&buff[2*(1*4)]);
+	l3 = _mm_load_ps(&buff[3*(1*4)]);
+	//_MM_TRANSPOSE4_PS(l0, l1, l2, l3);
+
+	// inputs
+	x0 = in0;
+	x1 = in1;
+
+	// shuffles
+	y0 = l0;
+	c0 = l1;
+	c1 = l2;
+	c2 = l3;
+	c3 = x0;
+
+	// operation
+	r3 = x1;
+	r2 = c3 + w3 * (l3 + r3);
+	r1 = c2 + w2 * (l2 + r2);
+	r0 = c1 + w1 * (l1 + r1);
+	y1 = c0 + w0 * (l0 + r0);
+
+	// update
+	l0 = r0;
+	l1 = r1;
+	l2 = r2;
+	l3 = r3;
+
+	// outputs
+	*out0 = y0;
+	*out1 = y1;
+
+	// store "L"
+	//_MM_TRANSPOSE4_PS(l0, l1, l2, l3);
+	_mm_store_ps(&buff[0*(1*4)], l0);
+	_mm_store_ps(&buff[1*(1*4)], l1);
+	_mm_store_ps(&buff[2*(1*4)], l2);
+	_mm_store_ps(&buff[3*(1*4)], l3);
+}
+#endif
+
 // ~ fdwt_cdf97_vert_cor4x4_sse_s
 #ifdef __SSE__
 static
@@ -213,6 +286,19 @@ do { \
 	vert_2x4((t2), (t3), &(t2), &(t3), (buff_v)); \
 	\
 	CORE_4X4_SCALE((t0), (t1), (t2), (t3)); \
+} while(0)
+
+#define CORE_4X4_CALC_INV(t0, t1, t2, t3, buff_h, buff_v) \
+do { \
+	CORE_4X4_SCALE((t0), (t1), (t2), (t3)); \
+	\
+	vert_2x4_inv((t0), (t1), &(t0), &(t1), (buff_h)); \
+	vert_2x4_inv((t2), (t3), &(t2), &(t3), (buff_h)); \
+	\
+	_MM_TRANSPOSE4_PS((t0), (t1), (t2), (t3)); \
+	\
+	vert_2x4_inv((t0), (t1), &(t0), &(t1), (buff_v)); \
+	vert_2x4_inv((t2), (t3), &(t2), &(t3), (buff_v)); \
 } while(0)
 
 #ifdef __SSE__
@@ -446,6 +532,64 @@ void unified_4x4(
 }
 
 static
+void unified_4x4_inv(
+	int x, int y,
+	int overlap_x_L, int size_x,
+	int overlap_y_L, int size_y,
+	const void *src_ptr,
+	int src_stride_x,
+	int src_stride_y,
+	void *dst_ptr,
+	int dst_stride_x,
+	int dst_stride_y,
+	void *buffer_x,
+	void *buffer_y
+)
+{
+	const int step_y = 4;
+	const int step_x = 4;
+
+	const int shift = 4; // vertical vectorization
+
+	__m128 t[4];
+
+	// CORE -- LOAD
+	for(int xx = 0; xx < step_x; xx++)
+	{
+		for(int yy = 0; yy < step_y; yy++)
+		{
+			// virtual => real coordinates
+			const int pos_x = virt2real(x, xx, overlap_x_L, size_x);
+			const int pos_y = virt2real(y, yy, overlap_y_L, size_y);
+
+			t[xx][yy] = *addr2_const_s(src_ptr, pos_y, pos_x, src_stride_x, src_stride_y);
+		}
+	}
+
+	// CORE -- CALC
+	CORE_4X4_CALC_INV(t[0], t[1], t[2], t[3], buffer_y, buffer_x);
+
+	// CORE -- STORE
+	for(int yy = 0; yy < step_y; yy++)
+	{
+		for(int xx = 0; xx < step_x; xx++)
+		{
+			// virtual => real coordinates
+#if 0
+			const int pos_x = virt2real(x-shift, xx, overlap_x_L, size_x);
+			const int pos_y = virt2real(y-shift, yy, overlap_y_L, size_y);
+#else
+			const int pos_x = virt2real_error(x-shift, xx, overlap_x_L, size_x);
+			const int pos_y = virt2real_error(y-shift, yy, overlap_y_L, size_y);
+			if( pos_x < 0 || pos_y < 0 )
+				continue;
+#endif
+			*addr2_s(dst_ptr, pos_y, pos_x, dst_stride_x, dst_stride_y) = t[yy][xx];
+		}
+	}
+}
+
+static
 void loop_shorted_4x4(
 	int base_x, int base_y,
 	int stop_x, int stop_y,
@@ -519,6 +663,52 @@ void loop_unified_4x4(
 		{
 #endif
 			unified_4x4(
+				x, y,
+				overlap_x_L, size_x,
+				overlap_y_L, size_y,
+				src_ptr, src_stride_x, src_stride_y,
+				dst_ptr, dst_stride_x, dst_stride_y,
+				buffer_x + x*buff_elem_size,
+				buffer_y + y*buff_elem_size
+			);
+		}
+	}
+}
+
+static
+void loop_unified_4x4_inv(
+	int base_x, int base_y,
+	int stop_x, int stop_y,
+	int step_x, int step_y,
+	int overlap_x_L, int size_x,
+	int overlap_y_L, int size_y,
+	const void *src_ptr,
+	int src_stride_x,
+	int src_stride_y,
+	void *dst_ptr,
+	int dst_stride_x,
+	int dst_stride_y,
+	float *buffer_x,
+	float *buffer_y
+)
+{
+	const int words = 1; // vertical
+	const int buff_elem_size = words*4;
+
+#if 1
+	// order=horizontal
+	for(int y = base_y; y < stop_y; y += step_y)
+	{
+		for(int x = base_x; x < stop_x; x += step_x)
+		{
+#else
+	// order=vertical
+	for(int x = base_x; x < stop_x; x += step_x)
+	{
+		for(int y = base_y; y < stop_y; y += step_y)
+		{
+#endif
+			unified_4x4_inv(
 				x, y,
 				overlap_x_L, size_x,
 				overlap_y_L, size_y,
@@ -819,6 +1009,81 @@ void cdf97_2f_dl_4x4_s(
 #endif /* one big loop */
 }
 
+// TODO: in-place transforms is not treated
+void cdf97_2i_dl_4x4_s(
+	int size_x,
+	int size_y,
+	const void *src_ptr,
+	int src_stride_x,
+	int src_stride_y,
+	void *dst_ptr,
+	int dst_stride_x,
+	int dst_stride_y
+)
+{
+	// TODO: assert
+
+	const int words = 1; // 1 = vertical vectorization
+	const int buff_elem_size = words*4;
+
+	// offset_x, offset_y
+	const int offset = 0; // FIXME: for FORWARD == 1
+
+	// step_x, step_y
+	const int step_y = 4;
+	const int step_x = 4;
+
+	// shift_x, shift_y
+	const int shift = 4; // 4 = vertical vectorization
+
+	const int modulo_x_L = (offset) % step_x;
+	const int overlap_x_L = step_x + !!modulo_x_L * (step_x-modulo_x_L); // (step_x-modulo_x_L) + (!!offset * step_x);
+	const int modulo_x_R = (overlap_x_L+size_x) % step_x;
+	const int overlap_x_R = step_x + !!modulo_x_R * (step_x-modulo_x_R); // step_x + ((modulo_x_R) ? (step_x-modulo_x_R) : (0));
+	const int super_x = overlap_x_L + size_x + overlap_x_R;
+// 	const int limit0_x = overlap_x_L + offset + shift;
+// 	const int limit1_x = overlap_x_L + size_x - modulo_x_R - step_x*!modulo_x_R; // HACK: last term should not be here
+
+// 	dwt_util_log(LOG_DBG, "mod_L=%i ovl_L=%i mod_R=%i ovl_R=%i => %i+%i+%i | limits: %i %i \n",
+// 		modulo_x_L, overlap_x_L, modulo_x_R, overlap_x_R,
+// 		overlap_x_L, size_x, overlap_x_R,
+// 		limit0_x, limit1_x
+//     		);
+
+	const int modulo_y_L = (offset) % step_y;
+	const int overlap_y_L = step_y + !!modulo_y_L * (step_y-modulo_y_L); // (step_x-modulo_x_L) + (!!offset * step_x);
+	const int modulo_y_R = (overlap_y_L+size_y) % step_y;
+	const int overlap_y_R = step_y + !!modulo_y_R * (step_y-modulo_y_R); // step_x + ((modulo_x_R) ? (step_x-modulo_x_R) : (0));
+	const int super_y = overlap_y_L + size_y + overlap_y_R;
+// 	const int limit0_y = overlap_y_L + offset + shift;
+// 	const int limit1_y = overlap_y_L + size_y - modulo_y_R - step_y*!modulo_y_R; // HACK: last term should not be here
+
+	// alloc buffers
+	float buffer_x[buff_elem_size*super_x] ALIGNED(16);
+	float buffer_y[buff_elem_size*super_y] ALIGNED(16);
+
+	// zero buffers
+#if 0
+	dwt_util_zero_vec_s(buffer_x, buff_elem_size*super_x);
+	dwt_util_zero_vec_s(buffer_y, buff_elem_size*super_y);
+#endif
+
+	// unified loop
+	{
+		loop_unified_4x4_inv(
+			/* base */ 0, 0,
+			/* stop */ super_x, super_y,
+			/* step */ step_x, step_y,
+			overlap_x_L, size_x,
+			overlap_y_L, size_y,
+			src_ptr, src_stride_x, src_stride_y,
+			dst_ptr, dst_stride_x, dst_stride_y,
+			buffer_x,
+			buffer_y
+		);
+	}
+}
+
 void dwt_util_perf_cdf97_2f_dl_4x4_s(
 	int size_x,
 	int size_y,
@@ -1099,7 +1364,65 @@ void dwt_cdf97_2f_dl_4x4_s(
 	}
 }
 
-// TODO
+void dwt_cdf97_2i_dl_4x4_s(
+	void *ptr,		///< pointer to beginning of image data
+	int stride_x,		///< difference between rows (in bytes)
+	int stride_y,		///< difference between columns (in bytes)
+	int size_o_big_x,	///< width of outer image frame (in elements)
+	int size_o_big_y,	///< height of outer image frame (in elements)
+	int size_x,		///< width of nested image (in elements)
+	int size_y,		///< height of nested image (in elements)
+	int j_max,		///< the number of intended decomposition levels (scales), the number of achieved decomposition levels will be stored also here
+	int decompose_one,	///< should be row or column of size one pixel decomposed? zero value if not
+	int zero_padding	///< fill padding in channels with zeros? zero value if not, should be non zero only for sparse decomposition
+)
+{
+	UNUSED(zero_padding);
+
+	const int size_o_big_min = min(size_o_big_x,size_o_big_y);
+	const int size_o_big_max = max(size_o_big_x,size_o_big_y);
+
+	int j = ceil_log2( decompose_one ? size_o_big_max : size_o_big_min );
+
+	if( j_max >= 0 && j_max < j )
+		j = j_max;
+
+	for(;;)
+	{
+		if( 0 == j )
+			break;
+
+		const int stride_y_j = mul_pow2(stride_y, j-1);
+		const int stride_x_j = mul_pow2(stride_x, j-1);
+
+		const int size_x_j = ceil_div_pow2(size_x, j-1);
+		const int size_y_j = ceil_div_pow2(size_y, j-1);
+
+		if( size_x_j < 8 || size_y_j < 8 )
+		{
+			// FIXME
+			dwt_util_error("unimplemented\n");
+		}
+		else
+		{
+// 			dwt_util_log(LOG_DBG, "j=%i: size=(%i,%i) stride=(%i,%i)\n", j, size_x_j, size_y_j, stride_x_j, stride_y_j);
+
+			cdf97_2i_dl_4x4_s(
+				size_x_j,
+				size_y_j,
+				ptr,
+				stride_x_j,
+				stride_y_j,
+				ptr,
+				stride_x_j,
+				stride_y_j
+			);
+		}
+
+		j--;
+	}
+}
+
 void dwt_util_perf_dwt_cdf97_2f_dl_4x4_s(
 	int size_x,
 	int size_y,
--- src/dwt-sym.h
+++ src/dwt-sym.h
@@ -26,6 +26,28 @@ void cdf97_2f_dl_4x4_s(
 );
 
 /**
+ * @brief Inverse DWT with CDF 9/7 over SP-FP, in-place subband organization.
+ *
+ * This function can work with in-place data (@p src == @p dst) as well as with not-in-place data (@p src != @p dst).
+ * The single-loop core approach is used in in the heart of the function.
+ * This approach is implemented using the @f$ 4\times4 @f$ core with vertical vectorization.
+ * The vectorized core is written using SSE instruction set.
+ * The image borders are extended using a virtual symmetric padding.
+ *
+ * @warning experimental
+ */
+void cdf97_2i_dl_4x4_s(
+	int size_x,
+	int size_y,
+	const void *src_ptr,
+	int src_stride_x,
+	int src_stride_y,
+	void *dst_ptr,
+	int dst_stride_x,
+	int dst_stride_y
+);
+
+/**
  * @brief Performance test.
  *
  * @warning experimental
@@ -74,6 +96,25 @@ void dwt_cdf97_2f_dl_4x4_s(
 	int size_x,		///< width of nested image (in elements)
 	int size_y,		///< height of nested image (in elements)
 	int *j_max_ptr,		///< pointer to the number of intended decomposition levels (scales), the number of achieved decomposition levels will be stored also here
+	int decompose_one,	///< should be row or column of size one pixel decomposed? zero value if not
+	int zero_padding	///< fill padding in channels with zeros? zero value if not, should be non zero only for sparse decomposition
+);
+
+/**
+ * @brief Inverse image fast wavelet transform using CDF 9/7 wavelet, in-place version.
+ *
+ * @note for compatibility with @ref dwt_cdf97_2i_inplace_s
+ * @warning experimental
+ */
+void dwt_cdf97_2i_dl_4x4_s(
+	void *ptr,		///< pointer to beginning of image data
+	int stride_x,		///< difference between rows (in bytes)
+	int stride_y,		///< difference between columns (in bytes)
+	int size_o_big_x,	///< width of outer image frame (in elements)
+	int size_o_big_y,	///< height of outer image frame (in elements)
+	int size_x,		///< width of nested image (in elements)
+	int size_y,		///< height of nested image (in elements)
+	int j_max,		///< the number of intended decomposition levels (scales), the number of achieved decomposition levels will be stored also here
 	int decompose_one,	///< should be row or column of size one pixel decomposed? zero value if not
 	int zero_padding	///< fill padding in channels with zeros? zero value if not, should be non zero only for sparse decomposition
 );
