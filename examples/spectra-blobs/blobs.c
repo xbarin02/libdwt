84f1b6d7ae340ce6968a61b27ab4c4209419fe05
David Barina <ibarina@fit.vutbr.cz>
2014-07-28 15:56:20 +0000

    examples: +spectra-blobs

---
+++ examples/spectra-blobs/blobs.c
@@ -0,0 +1,191 @@
+/**
+ * @brief Find strongest ridges of the S transform plane.
+ */
+
+#include "libdwt.h"
+#include <stddef.h> // size_t
+#include "image.h" // image_t, ...
+#include <complex.h> // complex
+#include "gabor.h" // gaussian_center
+#include "system.h" // dwt_util_free
+#include "util.h" // dwt_util_find_max_pos_s
+#include <assert.h> // assert
+#include <math.h> // sqrtf
+
+void remove_ridge(
+	image_t *ridges,
+	image_t *maxima
+)
+{
+	// for each y
+	for(int y = 0; y < maxima->size_y; y++)
+		// for each x
+		for(int x = 0; x < maxima->size_x; x++)
+			// when the point is a maximum
+			if( *image_coeff_s(maxima, y, x) > 0.f )
+				// remove the ridge
+#if 0
+				*image_coeff_s(ridges, y, x) = 0.f;
+#else
+				*image_coeff_s(ridges, y, x) *= 1.f - *image_coeff_s(maxima, y, x);
+#endif
+}
+
+static
+float s_sigma(float f)
+{
+	assert( f != 0.f );
+
+	const float alpha = f * f;
+	const float sigma = sqrtf(1.f/2.f/alpha);
+
+	return sigma;
+}
+
+// extern
+void s_gen_kernel(
+	float complex **ckern,
+	int stride,
+	float f
+);
+
+int get_maximum(
+	image_t *ridges,
+	image_t *maxima
+)
+{
+	int pos_x, pos_y;
+
+	// find maximum
+	dwt_util_find_max_pos_s(
+		// input
+		ridges->ptr,
+		ridges->size_x,
+		ridges->size_y,
+		ridges->stride_x,
+		ridges->stride_y,
+		// output
+		&pos_x,
+		&pos_y
+	);
+
+	dwt_util_log(LOG_INFO, "maximum found at (%i,%i)\n", pos_y, pos_x);
+
+	// add the surrounding pixels
+	{
+		float complex *ckern = 0;
+		int ckern_stride = sizeof(float complex);
+
+		// for each row
+		for(int y = 0; y < maxima->size_y; y++)
+		{
+			float norm1_y = (y+1.f)/(float)maxima->size_y;
+			float f = norm1_y * 0.5f;
+			float a = 1.f;
+			float sigma = s_sigma(f);
+			int center = gaussian_center(sigma, a);
+			int size = gaussian_size(sigma, a);
+
+			s_gen_kernel(&ckern, ckern_stride, f);
+			float kern[size];
+			dwt_util_vec_cabs_cs(kern, sizeof(float), ckern, ckern_stride, size);
+
+			float norm_factor = 1.f/kern[center];
+
+			for(int x = pos_x-center; x < pos_x-center+size; x++)
+			{
+				if( x < 0 || x >= maxima->size_x )
+					continue;
+
+				*dwt_util_addr_coeff_s(
+					maxima->ptr,
+					maxima->size_y-y-1,
+					x,
+					maxima->stride_x,
+					maxima->stride_y
+				) =
+#if 0
+				1.f;
+#else
+				kern[center+x-pos_x] * norm_factor;
+#endif
+			}
+		}
+
+		dwt_util_free(ckern);
+	}
+
+	remove_ridge(ridges, maxima);
+
+	return 1;
+}
+
+int main(int argc, char *argv[])
+{
+	dwt_util_init();
+
+	const char *path = (argc > 1) ? argv[1] :
+		"st/st-sum-class-all.mat";
+
+	struct image_t plane;
+
+	if( !image_load_from_mat_s(&plane, path) )
+		dwt_util_error("unable to load from %s\n", path);
+
+	dwt_util_log(LOG_INFO, "plane size=(%i,%i)\n", plane.size_y, plane.size_x);
+
+	image_save_to_pgm_s(&plane, "plane.pgm");
+
+	struct image_t ridges = (image_t){
+		.ptr = dwt_util_alloc_image2(plane.stride_x, plane.stride_y, plane.size_x, plane.size_y),
+		.size_x = plane.size_x,
+		.size_y = plane.size_y,
+		.stride_x = plane.stride_x,
+		.stride_y = plane.stride_y
+	};
+
+#if 1
+	detect_ridges1_s(
+		plane.ptr,
+		ridges.ptr,
+		plane.stride_x,
+		plane.stride_y,
+		plane.size_x,
+		plane.size_y,
+		0.f
+	);
+#else
+	ridges.ptr = plane.ptr;
+#endif
+
+	image_save_to_pgm_s(&ridges, "ridges.pgm");
+
+	struct image_t maxima = (image_t){
+		.ptr = dwt_util_alloc_image2(plane.stride_x, plane.stride_y, plane.size_x, plane.size_y),
+		.size_x = plane.size_x,
+		.size_y = plane.size_y,
+		.stride_x = plane.stride_x,
+		.stride_y = plane.stride_y
+	};
+
+	// strongest ridges
+	for(int i = 0; i < 20; i++)
+	{
+		dwt_util_log(LOG_INFO, "extracting ridge %i...\n", i);
+
+		image_zero(&maxima);
+
+		get_maximum(&ridges, &maxima);
+
+		image_save_to_pgm_format_s(&maxima, "maxima-%i.pgm", i);
+		image_save_to_pgm_format_s(&ridges, "ridges-%i.pgm", i);
+	}
+
+	image_free(&maxima);
+	image_free(&ridges);
+	image_free(&plane);
+
+	dwt_util_finish();
+
+	return 0;
+}
