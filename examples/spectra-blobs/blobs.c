b6d77be21f4f33ae8ac470085c5c4ba6542e7a9f
David Barina <ibarina@fit.vutbr.cz>
2014-07-29 10:46:46 +0000

    examples/spectra-blobs: minor update

--- examples/spectra-blobs/blobs.c
+++ examples/spectra-blobs/blobs.c
@@ -191,14 +191,14 @@ int main(int argc, char *argv[])
 	const char *path = (argc > 1) ? argv[1] :
 		"data/st-log/st-sum-class-all.mat";
 
-	struct image_t plane;
+	image_t *plane = image_create_from_mat_s(path);
 
-	if( !image_load_from_mat_s(&plane, path) )
+	if( !plane )
 		dwt_util_error("unable to load from %s\n", path);
 
-	dwt_util_log(LOG_INFO, "plane size=(%i,%i)\n", plane.size_y, plane.size_x);
+	dwt_util_log(LOG_INFO, "plane size=(%i,%i)\n", plane->size_y, plane->size_x);
 
-	image_save_to_pgm_s(&plane, "plane.pgm");
+	image_save_to_pgm_s(plane, "plane.pgm");
 
 	const int ridges_no = 20;
 
@@ -206,15 +206,13 @@ int main(int argc, char *argv[])
 	image_t *points = image_create_s(2, ridges_no);
 
 	// strongest ridges
-	spectra_st_get_strongest_ridges(&plane, points, ridges_no);
+	spectra_st_get_strongest_ridges(plane, points, ridges_no);
 
 	image_save_to_mat_s(points, "points.mat");
 
-	// TODO: image_destroy
-	image_free(points); // free image->ptr
-	dwt_util_free(points); // free image
+	image_destroy(points);
 
-	image_free(&plane);
+	image_destroy(plane);
 
 	dwt_util_finish();
 
